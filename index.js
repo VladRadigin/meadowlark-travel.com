5f96f7fd09f94bd1a3b231182dfb10ce3d4f89e6
VladRadigin <angry.gamer.comi@gmail.com>
2021-11-02 14:41:10 +0300

    Adding cookies with a random prediction

--- index.js
+++ index.js
@@ -11,9 +11,21 @@ app.engine('handlebars', expressHandlebars({
 app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => res.render('home'));
+const fortunes = [
+  "Победи свои страхи, или они победят тебя.",
+  "Рекам нужны истоки.",
+  "Не бойся неведомого.",
+  "Тебя ждет приятный сюрприз.",
+  "Будь проще везде, где только можно.",
+]
 
-app.get('/about', (req, res) => res.render('about'));
+
+app.get('/', (req, res) => res.render('home'));
+app.get('/about', (req, res) => {
+  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render('about', { fortune: randomFortune });
+});
+
 
 // Пользовательская страница 404
 app.use((req, res) => {
@@ -25,7 +37,7 @@ app.use((req, res) => {
 app.use((err, req, res, next) => {
   console.error(err.message);
   res.status(500);
-  res.send('500');
+  res.render('500');
 })
 
 app.listen(port, () => console.log(`Express запущен на http://localhost:${port}; ` + `нажмите Ctrl+C для заверщения.`));
